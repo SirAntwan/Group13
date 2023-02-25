dadd0a730b053917acae01808553b08a944ef98f
SirAntwan <124811350+SirAntwan@users.noreply.github.com>
2023-02-24 21:20:57 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,50 @@
+<?php 
+
+session_start();
+
+if (isset($_SESSION['UniqueID']) && isset($_SESSION['Username'])) {
+
+ ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>HOME</title>
+
+    <link rel="stylesheet" type="text/css" href="Style.css">
+
+</head>
+<script>
+     function MyAccountPage() {
+			document.location.href = "MyAccount.php";
+		}
+</script>
+<body>
+     <h1>Hello, <?php echo $_SESSION['Username']; ?></h1>
+
+     <a href="MyAccount.php">My Account</a>
+
+     <body>
+          &nbsp;
+     </body>
+
+     <a href="logout.php">Logout</a>
+
+</body>
+
+</html>
+
+<?php 
+
+}else{
+
+     header("Location: index.php");
+
+     exit();
+
+}
+
+ ?>
