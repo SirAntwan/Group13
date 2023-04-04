989f599bcf09d5ed625525076b0917bf425bd893
Sanguines0 <110374534+Sanguines0@users.noreply.github.com>
2023-04-04 15:15:25 -0400

    Add files via upload

---
+++ DB_Inserts.sql
@@ -0,0 +1,2 @@
+INSERT INTO 'songs' ('songID', 'songName', 'genre', 'artist', 'songLength', 'ratings', 'releaseYear') VALUES
+('1', 'Starboy', 'Alternative R&B', 'The Weeknd', 230, '9.1/10', 2016);
---
+++ DB_Tables.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2023 at 09:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `songs`
+--
+
+CREATE TABLE `songs` (
+  `songID` varchar(5) NOT NULL,
+  `songName` varchar(50) NOT NULL,
+  `genre` varchar(50) NOT NULL,
+  `artist` varchar(50) NOT NULL,
+  `songLength` int(4) NOT NULL,
+  `ratings` varchar(10) NOT NULL,
+  `releaseYear` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`songID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
